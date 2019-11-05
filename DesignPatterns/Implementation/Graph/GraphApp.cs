2ed4477e2c2ac0be6e815e7e4fad6c14db8cd037
clgray <clgray@yandex.ru>
2019-11-06 00:00:01 +0300

    fix GraphApp

--- DesignPatterns/Implementation/Graph/GraphApp.cs
+++ DesignPatterns/Implementation/Graph/GraphApp.cs
@@ -22,7 +22,7 @@ namespace Implementation.Graph
 			graf[8].ChildNodes = new[] { graf[0] };
 			graf[6].ChildNodes = new[] { graf[4] };
 
-			var g = new Graph(graf.ToArray(), 0);
+			var g = new Graph(graf.ToArray(), graf[0]);
 			foreach (Node item in g)
 			{
 				Console.WriteLine(item.Id);
