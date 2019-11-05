8e8e6aaeef3eb2a943968bad452c8711b840f3af
clgray <clgray@yandex.ru>
2019-11-05 23:58:36 +0300

    Reverse dfs

--- DesignPatterns/Implementation/Graph/Graph.cs
+++ DesignPatterns/Implementation/Graph/Graph.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Implementation.Graph
 {
@@ -9,12 +10,12 @@ namespace Implementation.Graph
 		readonly HashSet<int> _visited = new HashSet<int>();
 		Node _current;
 		readonly Node[] _graph;
-		readonly int _startId;
+		readonly Node _startNode;
 
-		public Graph(Node[] graph, int startId)
+		public Graph(Node[] graph, Node startNode)
 		{
 			_graph = graph;
-			_startId = startId;
+			_startNode = startNode;
 		}
 
 		public IEnumerator GetEnumerator()
@@ -25,43 +26,39 @@ namespace Implementation.Graph
 
 		public bool MoveNext()
 		{
-			if (_current == null)
+			while (true)
 			{
-				_current = _graph[_startId];
-				_visited.Add(_current.Id);
-
-				return true;
-			}
-			if (_current.ChildNodes != null)
-			{
-				foreach (var child in _current.ChildNodes)
+				if (_mem.Count > 0)
 				{
-					if (!_visited.Contains(child.Id))
+					var node = _mem.Pop();
+					if (_visited.Contains(node.Id)) continue;
+
+					_current = node;
+					_visited.Add(_current.Id);
+
+					if (_current.ChildNodes != null)
 					{
-						_mem.Push(child);
-						_visited.Add(child.Id);
+						foreach (var child in _current.ChildNodes.Reverse())
+						{
+							if (!_visited.Contains(child.Id))
+							{
+								_mem.Push(child);
+							}
+						}
 					}
+					return true;
 				}
+				return false;
 			}
-
-			if (_mem.Count > 0)
-			{
-				_current = _mem.Pop();
-				return true;
-			}
-
-			return false;
 		}
 
 		public void Reset()
 		{
-			
+			_mem.Clear();
+			_mem.Push(_startNode);
 			_visited.Clear();
 		}
 
-		public object Current
-		{
-			get { return _current; }
-		}
+		public object Current => _current;
 	}
 }
